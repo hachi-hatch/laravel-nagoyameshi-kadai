3854c7d63e8dfe4d56585ea73806e6c0eb080618
hachi-hatchi <yr78.kim@gmail.com>
2025-07-12 15:18:47 +0900

    コードの不要部分を削除

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -10,53 +10,6 @@ class RestaurantController extends Controller
 {
     public function index(Request $request)
     {
-        // $keyword = $request->keyword;
-        // $category_id = $request->category;
-        // $price = $request->price;
-        // $selected_sort = $request->select_sort;
-
-        // $sorts = [
-        //     '掲載日が新しい順' => 'created_at desc',
-        //     '価格が安い順' => 'lowest_price asc'
-        // ];
-
-        // $sort_query = [];
-        // $sorted = 'created_at desc';
-
-        // if (!empty($selected_sort) && isset($sorts[$selected_sort])) {
-        //     $sorted = $sorts[$selected_sort];
-        // }
-
-        // [$column, $direction] = explode(' ', $sorted);
-
-        // $query = Restaurant::query();
-
-        // if (!empty($keyword)) {
-        //     $query->where(function ($q) use ($keyword) {
-        //         $q->where('name', 'like', "%{$keyword}%")
-        //         ->orWhere('address', 'like', "%{$keyword}%")
-        //         ->orWhereHas('categories', function ($q2) use ($keyword) {
-        //             $q2->where('name', 'like', "%{$keyword}%");
-        //         });
-        //     });
-        // }
-
-        // if (!empty($category_id)) {
-        //     $query->whereHas('categories', function($q) use ($category_id) {
-        //         $q->where('id', $category_id);
-        //     });
-        // }
-
-        // if (!empty($price)) {
-        //     $query->where('lowest_price', '<=', $price);
-        // }
-
-        // $query->orderBy($column, $direction);
-        
-        // $restaurants = $query->paginate(15)->appends($request->all()); 
-        // $categories = Category::all();
-        // $total = $restaurants->total();
-
         $keyword = $request->keyword;
         $category_id = $request->category_id;
         $price = $request->price;
